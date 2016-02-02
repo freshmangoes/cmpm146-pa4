d3a0251cfb021cb71d39a013da18d04865010e75
Kyle <kylecilia@gmail.com>
2016-02-01 23:22:59 -0800

    Commenting out unused code

--- behavior_tree_bot/behaviors.py
+++ behavior_tree_bot/behaviors.py
@@ -34,20 +34,20 @@ def attack_improved(state):
     pass
 
 
-def spread_to_weakest_neutral_planet(state):
-    # (1) If we currently have a fleet in flight, just do nothing.
-    #if len(state.my_fleets()) >= 1:
-    #    return False
-    # (2) Find my strongest planet.
-    strongest_planet = max(state.my_planets(), key=lambda p: p.num_ships, default=None)
-    # (3) Find the weakest neutral planet.
-    weakest_planet = min(state.neutral_planets(), key=lambda p: p.num_ships, default=None)
-    if not strongest_planet or not weakest_planet:
-        # No legal source or destination
-        return False
-    else:
-        # (4) Send half the ships from my strongest planet to the weakest enemy planet.
-        return issue_order(state, strongest_planet.ID, weakest_planet.ID, strongest_planet.num_ships / 2)
+# def spread_to_weakest_neutral_planet(state):
+#     # (1) If we currently have a fleet in flight, just do nothing.
+#     #if len(state.my_fleets()) >= 1:
+#     #    return False
+#     # (2) Find my strongest planet.
+#     strongest_planet = max(state.my_planets(), key=lambda p: p.num_ships, default=None)
+#     # (3) Find the weakest neutral planet.
+#     weakest_planet = min(state.neutral_planets(), key=lambda p: p.num_ships, default=None)
+#     if not strongest_planet or not weakest_planet:
+#         # No legal source or destination
+#         return False
+#     else:
+#         # (4) Send half the ships from my strongest planet to the weakest enemy planet.
+#         return issue_order(state, strongest_planet.ID, weakest_planet.ID, strongest_planet.num_ships / 2)
 
 
 def defensive_close(state):
